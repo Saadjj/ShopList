1d2cac8ed1aa42ba151c80cf9f17f88214f2ef60
ilev <ilev.andrei@yandex.ru>
2022-07-22 17:03:00 +0300

    отклик на свайп

--- app/src/main/java/com/bignerdranch/android/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/shoplist/presentation/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.shoplist.R
 import com.bignerdranch.android.shoplist.domain.ShopItem
@@ -18,6 +19,28 @@ class MainActivity : AppCompatActivity() {
 
     // ссылка на адаптер
     private lateinit var shopListadapter: ShopListAdapter
+
+    //    добавление отклика на свайп
+    val callback =
+        object : ItemTouchHelper.SimpleCallback(
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val item = shopListadapter.shopList[viewHolder.adapterPosition]
+                viewModel.deleteShopItem(item)
+            }
+        }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,12 +72,12 @@ class MainActivity : AppCompatActivity() {
             ShopListAdapter.MAX_POOL_SIZE
         )
 //место где твориться магия и происходит вызов метода, меняющего аттрибут доступности у шопитема
-        shopListadapter.onShopItemLongClickListener ={
+        shopListadapter.onShopItemLongClickListener = {
             viewModel.changeEnableState(it)
         }
-       shopListadapter.onShopItemClickListener={
-           Log.d("MainActivity",it.toString())
-       }
+        shopListadapter.onShopItemClickListener = {
+            Log.d("MainActivity", it.toString())
+        }
     }
 
 }
