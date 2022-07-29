edfbcc7b5a7a6d817d6c386479f8d6b0fe9ff085
ilev <ilev.andrei@yandex.ru>
2022-07-29 17:46:56 +0300

    viewModel

---
+++ app/src/main/java/com/bignerdranch/android/shoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.shoplist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bignerdranch.android.shoplist.R
+
+class ShopItemActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
