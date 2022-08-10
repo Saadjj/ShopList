868a8b48e804b9b513dd8e9fd9cb29394bf1ac9d
Ilev Andrei <ilev.andrei@yandex.ru>
2022-08-10 21:58:41 +0300

    10/08

--- app/src/main/java/com/bignerdranch/android/shoplist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/bignerdranch/android/shoplist/presentation/ShopItemActivity.kt
@@ -9,38 +9,78 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bignerdranch.android.shoplist.R
+import com.bignerdranch.android.shoplist.domain.ShopItem
 import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ShopItemViewModel
 
-    private lateinit var tilName:TextInputLayout
-    private lateinit var tilCount:TextInputLayout
-    private lateinit var etName:EditText
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
     private lateinit var etCount: EditText
-    private lateinit var button: Button
+    private lateinit var buttonSave: Button
+
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        //проверка
+        parseIntent()
         //инициализация вьюмодели
         viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        //инициализация вьюшек
         initViews()
-        //присвоение полученных значений
+        //настройка экрана, еслижо этоговсе ок
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+//подписаться на нужын еобъеты лайвдаты
+    private fun launchEditMode() {
+        //TODO
+    }
+
+    private fun launchAddMode() {
+        //TODO
+    }
+
+
+    /**
+     * проверяем входящие параметры интента
+     */
+    private fun parseIntent() {
+        if (intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent")
+        }
         val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        Log.d("ShopItemActivity", mode.toString())
+        if (mode != MODE_ADD && mode != MODE_EDIT) {
+            throw RuntimeException("Param screen mode is unknown $mode")
+        }
+        screenMode = mode
+
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Param EXTRA_SHOP_ITEM_ID is absent")
+            }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+
+        }
     }
 
     /**
      * инициализация вьюшек
      */
-    private fun initViews(){
-        tilName=findViewById(R.id.til_name)
-        tilCount=findViewById(R.id.til_count)
-        etName=findViewById(R.id.et_name)
-        etCount=findViewById(R.id.et_count)
-        button==findViewById(R.id.save_button)
+    private fun initViews() {
+        tilName = findViewById(R.id.til_name)
+        tilCount = findViewById(R.id.til_count)
+        etName = findViewById(R.id.et_name)
+        etCount = findViewById(R.id.et_count)
+        buttonSave = findViewById(R.id.save_button)
 
     }
 
@@ -49,6 +89,7 @@ class ShopItemActivity : AppCompatActivity() {
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
         private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_UNKNOWN = ""
 
         /**
          * добавление экрана приложения с добавлением списка покупок
@@ -58,16 +99,17 @@ class ShopItemActivity : AppCompatActivity() {
             //кладем в интент необходимые параметры
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
             return intent
+
         }
 
         /**
          * добавление экрана приложения с редактированием списка покупок
          */
-        fun newIntentEditItem(context: Context, shopItemId:Int): Intent {
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
 
-            val intent=Intent(context, ShopItemActivity::class.java)
+            val intent = Intent(context, ShopItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
-            intent.putExtra(EXTRA_SHOP_ITEM_ID,shopItemId)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
             return intent
         }
     }
